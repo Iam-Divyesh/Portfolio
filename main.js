cc24cc2b4dbef5525a6b2ba82fc9b8dbd576e5fd
Divyesh_Sarvaiya <152177498+divyesh1012@users.noreply.github.com>
2023-12-12 10:35:11 +0000

    Home and About

---
+++ main.js
@@ -0,0 +1,13 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontent = document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
